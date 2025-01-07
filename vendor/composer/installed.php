53a7350ebe22924fde6bc20c0ebbf1155fb5653b
Amin - Ayoub <ayoub.nadir@etu.univlyon1.fr>
2025-01-07 12:24:26 +0100

    Add initial Composer configuration and autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'p2101440/projet',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b40fbfccd5a55de9bb4fbdd3a14c4e558936809b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'p2101440/projet' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b40fbfccd5a55de9bb4fbdd3a14c4e558936809b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
